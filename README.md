13f67684427bdb8d6d1ad2e297ec562e4482d814
Evan Anders <evanhanders@gmail.com>
2020-05-18 16:10:36 -0600

    makes plotpal installable

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dedalus_plotting
 Post-processing tools for plotting results from Dedalus simulations.
+
+# Installation
+To install plotpal on your local machine, navigate to the root plotpal/ directory (where setup.py is located), and type:
+
+> pip3 install -e .
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='plotpal',
+      version='0.1',
+      description='A plugin for analyzing Dedalus simulations',
+      url='https://github.com/evanhanders/plotpal',
+      author='Evan Anders',
+      author_email='evan.anders@colorado.edu',
+      license='GPL-3.0',
+      packages=['plotpal'],
+      zip_safe=False)
