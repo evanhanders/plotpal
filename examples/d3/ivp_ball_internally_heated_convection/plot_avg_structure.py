333769d14f1deadc28c79dcfdc5aae81a4e2b4f1
Evan Anders <evanhanders@gmail.com>
2023-04-06 17:02:05 -0500

    Updates profile plots in ball example

--- examples/d3/ivp_ball_internally_heated_convection/plot_avg_structure.py
+++ examples/d3/ivp_ball_internally_heated_convection/plot_avg_structure.py
@@ -2,39 +2,32 @@
 This script plots the average temperature structure of the simulation, averaged over a specified number of writes.
 
 Usage:
-    plot_avg_structure.py <root_dir> [options]
+    plot_avg_structure.py [options]
 
 Options:
-    --data_dir=<dir>                    Name of data handler directory [default: profiles]
-    --subdir_name=<subdir_name>               Name of figure output directory & base name of saved figures [default: avg_structure]
-    --start_file=<file_start_num>       Number of Dedalus output file to start plotting at [default: 1]
-    --n_files=<num_files>               Total number of files to plot
-    --writes_per_avg=<int>              Number of writes over which to take average [default: 15]
-    --dpi=<dpi>                         Image pixel density [default: 200]
+    --root_dir=<str>           Path to root directory containing data_dir [default: .]
+    --data_dir=<str>           Name of data handler directory [default: profiles]
+    --subdir_name=<str>        Name of figure output directory & base name of saved figures [default: avg_structure]
+    --start_file=<int>         Number of Dedalus output file to start plotting at [default: 1]
+    --n_files=<int>            Total number of files to plot
+    --writes_per_avg=<int>     Number of writes over which to take average [default: 50]
+    --dpi=<int>                Image pixel density [default: 200]
 
-    --fig_width=<in>                    Figure width (inches) [default: 6]
-    --fig_height=<in>                   Figure height (inches) [default: 3]
+    --fig_width=<float>        Figure width (inches) [default: 6]
+    --fig_height=<float>       Figure height (inches) [default: 3]
 """
 from docopt import docopt
 args = docopt(__doc__)
 from plotpal.profiles import AveragedProfilePlotter
 
-# Read in master output directory
-root_dir    = args['<root_dir>']
+root_dir    = args['--root_dir']
 data_dir    = args['--data_dir']
-if root_dir is None:
-    print('No dedalus output dir specified, exiting')
-    import sys
-    sys.exit()
-
-# Read in additional plot arguments
 start_file  = int(args['--start_file'])
 writes_per_avg = int(args['--writes_per_avg'])
 subdir_name    = args['--subdir_name']
 n_files     = args['--n_files']
 if n_files is not None: 
     n_files = int(n_files)
-
 
 # Create Plotter object, tell it which fields to plot
 plotter = AveragedProfilePlotter(root_dir, writes_per_avg=writes_per_avg, file_dir=data_dir, out_name=subdir_name, start_file=start_file, n_files=n_files)
--- examples/d3/ivp_ball_internally_heated_convection/plot_rolled_structure.py
+++ examples/d3/ivp_ball_internally_heated_convection/plot_rolled_structure.py
@@ -2,33 +2,27 @@
 This script plots the average temperature structure of the simulation, averaged over a specified number of writes.
 
 Usage:
-    plot_avg_structure.py <root_dir> [options]
+    plot_rolled_structure.py [options]
 
 Options:
-    --data_dir=<dir>                    Name of data handler directory [default: profiles]
-    --subdir_name=<subdir_name>               Name of figure output directory & base name of saved figures [default: rolled_structure]
-    --start_file=<file_start_num>       Number of Dedalus output file to start plotting at [default: 1]
-    --n_files=<num_files>               Total number of files to plot
-    --roll_writes=<int>                 Number of writes over which to take average
-    --dpi=<dpi>                         Image pixel density [default: 200]
+    --root_dir=<str>           Path to root directory containing data_dir [default: .]
+    --data_dir=<str>           Name of data handler directory [default: profiles]
+    --subdir_name=<str>        Name of figure output directory & base name of saved figures [default: rolled_structure]
+    --start_file=<int>         Number of Dedalus output file to start plotting at [default: 1]
+    --n_files=<int>            Total number of files to plot
+    --roll_writes=<int>        Number of writes over which to take average
+    --dpi=<int>                Image pixel density [default: 200]
 
-    --col_inch=<in>                    Figure width (inches) [default: 6]
-    --row_inch=<in>                   Figure height (inches) [default: 3]
+    --col_inch=<float>         Subplot width (inches) [default: 6]
+    --row_inch=<float>         Subplot height (inches) [default: 3]
 """
 from docopt import docopt
 args = docopt(__doc__)
 from plotpal.profiles import RolledProfilePlotter
 from plotpal.file_reader import match_basis
 
-# Read in master output directory
-root_dir    = args['<root_dir>']
+root_dir    = args['--root_dir']
 data_dir    = args['--data_dir']
-if root_dir is None:
-    print('No dedalus output dir specified, exiting')
-    import sys
-    sys.exit()
-
-# Read in additional plot arguments
 start_file  = int(args['--start_file'])
 subdir_name    = args['--subdir_name']
 n_files     = args['--n_files']
@@ -39,16 +33,15 @@ roll_writes = args['--roll_writes']
 if roll_writes is not None:
     roll_writes = int(roll_writes)
 
-def inv_T(ax, dictionary, index):
+def tot_lum(ax, dictionary, index):
     r = match_basis(dictionary['T profile'], 'r')
-    ax.plot(r, 1/dictionary['T profile'][index].ravel(), c='orange', ls='--')
+    ax.plot(r, dictionary['conv luminosity'][index].ravel() + dictionary['cond luminosity'][index].ravel(), c='black', label='total luminosity')
     
-
 # Create Plotter object, tell it which fields to plot
 plotter = RolledProfilePlotter(root_dir, file_dir=data_dir, out_name=subdir_name, roll_writes=roll_writes, start_file=start_file, n_files=n_files)
 plotter.setup_grid(num_rows=2, num_cols=1, col_inch=float(args['--col_inch']), row_inch=float(args['--row_inch']))
 plotter.add_line('r', 'T profile', grid_num=0)
-plotter.add_line('r', inv_T, grid_num=0, needed_tasks=['T profile',])
+plotter.add_line('r', tot_lum, grid_num=1, needed_tasks=['conv luminosity', 'cond luminosity'])
 plotter.add_line('r', 'conv luminosity', grid_num=1)
 plotter.add_line('r', 'cond luminosity', grid_num=1)
 plotter.plot_lines()
